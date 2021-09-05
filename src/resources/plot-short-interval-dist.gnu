9fd850958c56b5df99f8da9fc5c422d9c67e284a
boranorben <zhotkp@gmail.com>
2021-09-06 00:06:08 +0900

    :zap: add short interval cnt plot

---
+++ src/resources/plot-short-interval-dist.gnu
@@ -0,0 +1,12 @@
+set term pdfcairo size 8.5,8.5 font "Times-New-Roman, 25"
+set xlabel "Switching period (minute)"
+set ylabel "Total distance (kilometer)"
+set grid
+set key outside
+
+set output "short-interval-dist.pdf"
+plot "short-interval-dist-2.dat" using 1:2 w lp lw 2 title "2 Trucks",\
+"short-interval-dist-3.dat" using 1:2 w lp lw 2 title "3 Trucks",\
+"short-interval-dist-4.dat" using 1:2 w lp lw 2 title "4 Trucks",\
+"short-interval-dist-5.dat" using 1:2 w lp lw 2 title "5 Trucks",\
+"short-interval-dist-6.dat" using 1:2 w lp lw 2 title "6 Trucks"
--- src/resources/run.sh
+++ src/resources/run.sh
@@ -14,9 +14,10 @@ do
 done
 
 # Analysis
-cat output* | grep -e 'wo.*30' -e 'wo.*60' -e 'wo.*120' | awk '{print $2, $3, $4, $5}' > without-sts-cnt.dat
-cat output* | grep -e 'with.*30' -e 'with.*60' -e 'with.*120' | awk '{print $2, $3, $4, $5}' > with-sts-cnt.dat
-cat output* | grep -e 'wo.*10' -e 'wo.* 20' -e 'wo.*30' -e 'wo.*40' -e 'wo.*50' -e 'wo.*60' | awk '{print $2, $3, $4, $5}' > with-sts-dist.dat
+cat output* | grep -e 'wo.* 30' -e 'wo.* 60' -e 'wo.* 120' | awk '{print $2, $3, $4, $5}' > without-sts-cnt.dat
+cat output* | grep -e 'with.* 30' -e 'with.* 60' -e 'with.* 120' | awk '{print $2, $3, $4, $5}' > with-sts-cnt.dat
+cat output* | grep -e 'wo.* 10' -e 'wo.* 20' -e 'wo.* 30' -e 'wo.* 40' -e 'wo.* 50' -e 'wo.* 60' | awk '{print $2, $3, $4, $5}' > short-interval-dist.dat
+cat output* | grep -e 'with.* 10' -e 'with.* 20' -e 'with.* 30' -e 'with.* 40' -e 'with.* 50' -e 'with.* 60' | grep -v '3910 20' | awk '{print $2, $3, $4, $5}' > short-interval-cnt.dat
 
 for duration in ${durationlist[@]}
 do
@@ -29,9 +30,11 @@ done
 
 for platoon in ${platoonlist[@]}
 do
-	cat with-sts-dist.dat | grep "^$platoon" | awk '{print $2, $3, $4}' > with-sts-dist-$platoon.dat
+	cat short-interval-dist.dat | grep "^$platoon" | awk '{print $2, $3, $4}' > short-interval-dist-$platoon.dat
+	cat short-interval-cnt.dat | grep "^$platoon" | awk '{print $2, $3, $4}' > short-interval-cnt-$platoon.dat
 done
 
 cat plot-sts-cnt.gnu | gnuplot
-cat plot-sts-dist.gnu | gnuplot
+cat plot-short-interval-dist.gnu | gnuplot
+cat plot-short-interval-cnt.gnu | gnuplot
 rm -rf *.dat
